938408346984ea5a38b0886b078e557f7d16edb4
lay423 <junpang486@gmail.com>
2023-07-12 21:15:35 +0900

    프로젝트 생성

---
+++ src/main/java/com/chargersearchservice/ChargerSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chargersearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargerSearchServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChargerSearchServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/chargersearchservice/ChargerSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chargersearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargerSearchServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
